232b2cc4998f77b7cb877762ef5c089aa777c61d
manisaparajuli <manisaparajuli66@gmail.com>
2024-05-19 15:59:45 +1000

    fetched data on btn click

--- src/components/DataFetch.jsx
+++ src/components/DataFetch.jsx
@@ -2,26 +2,34 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
 function DataFetch() {
-  const [post, setPost] = useState([])
+  const [post, setPost] = useState({})
+  const [id, setId] = useState(1)
+  const[idFromButton, setIdFromButton] = useState(1)
+
+  const handleClick = () => {
+    setIdFromButton(id)
+  }
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts').then( res => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButton}`).then( res => {
       console.log(res)
       setPost(res.data)
     })
     .catch(err => {
       console.log(err)
     })
-  }, [])
+  }, [idFromButton])
   return (
     <div>
-      <input type="text" />
-      <ul>
+      <input type="text" value={id} onChange={e => setId(e.target.value)}/>
+      <button type='button' onClick={handleClick}>Fetch post title</button>
+      <p>{post.title}</p>
+      {/* <ul>
         {
           post.map(p => (
             <li key={p.id}> hello {p.title}</li>
           ))
         }
-      </ul>
+      </ul> */}
     </div>
   )
 }
